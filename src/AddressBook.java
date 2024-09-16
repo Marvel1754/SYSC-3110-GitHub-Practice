50a65197d0c923fb64d864b01aa236ddbbd94b2e
Marvel <m4rvelados1000@gmail.com>
2024-09-16 08:14:12 -0400

    Added a new AddressBook object and added and removed a buddy from that AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,27 +1,34 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-    private ArrayList<BuddyInfo> buddyInfos = new ArrayList<BuddyInfo>();
+    private ArrayList<BuddyInfo> buddyInfos;
 
+    public AddressBook() {
+        buddyInfos = new ArrayList<BuddyInfo>();
+    }
 
     /**
      * Adds a Buddy to the list of buddies in the address book.
-     * @param name: Name of the new buddy.
-     * @param address: Address of the new buddy.
-     * @param number: Phone number of the new buddy.
+     * @param buddy: BuddyInfo object to add a buddy.
      */
-    public void addBuddy(String name, String address, long number) {
-
+    public void addBuddy(BuddyInfo buddy) {
+        buddyInfos.add(buddy);
     }
 
     /**
      * Remove a buddy from the address book.
      */
-    public void removeBuddy() {
-
+    public void removeBuddy(BuddyInfo buddy) {
+        buddyInfos.remove(buddy);
     }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+
+        AddressBook myBuddies = new AddressBook();
+        BuddyInfo aBuddy = new BuddyInfo();
+        myBuddies.addBuddy(aBuddy);
+        myBuddies.removeBuddy(aBuddy);
+
     }
 }
