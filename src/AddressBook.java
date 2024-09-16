ac9dec66362e8663d3b15ab6f33cdf60d9e22030
Marvel <m4rvelados1000@gmail.com>
2024-09-16 07:57:31 -0400

    Added main method printing 'Address Book'

---
+++ src/AddressBook.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyInfos = new ArrayList<BuddyInfo>();
+
+
+    /**
+     * Adds a Buddy to the list of buddies in the address book.
+     * @param name: Name of the new buddy.
+     * @param address: Address of the new buddy.
+     * @param number: Phone number of the new buddy.
+     */
+    public void addBuddy(String name, String address, long number) {
+
+    }
+
+    /**
+     * Remove a buddy from the address book.
+     */
+    public void removeBuddy() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
